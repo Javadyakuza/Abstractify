7d67d86e088d24b7db815cec8cd62937975ba1a9
Javad Yakuzaa <javadyakuzaa@gmail.com>
2023-08-18 13:19:47 +0330

    initial commit

---
+++ contracts/SmartAccount.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "hardhat/console.sol";
+
+contract SmartAccount {
+    // static variables //
+    // authorization functions //
+    // transaction delegation functions //
+    // gas handling functions //
+    // owner functions //
+    // gaurdian functions //
+}
---
+++ contracts/interfaces/ISamrtAccountfactory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+interface ISmartAccountFactory {
+    // static variables //
+    // owner functions //
+    // deploying SmartAccount functions //
+}
